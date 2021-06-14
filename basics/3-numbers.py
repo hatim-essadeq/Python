210547312c56be2418c039bef564eddcc7d886c7
hatim essadeq <hatim.essadeq@gmail.com>
2021-06-14 11:25:51 +0100

    basics/3-numbers

---
+++ basics/3-numbers.py
@@ -0,0 +1,44 @@
+# normal number
+print(4)
+
+# decimal number
+print(16.04)
+
+# negative number
+print(-16.04)
+
+# add - dub - mul - div
+print(16 + 4)
+print(16 - 4)
+print(16 * 4)
+print(16 / 4)
+
+# modules
+print(16 % 3)
+
+# absolute value
+print(abs(-16))
+
+# power
+print(pow(3,2)) # 3^2
+
+# max and min of list numbers
+print(max(4,7,3))
+print(min(4,7,3))
+
+# round of a number if >0.5 (+1) else (-1)
+print(round(14.6)) # -> 15
+print(round(14.4)) # -> 14
+print(round(14.5)) # -> 14
+
+################### using math library
+from math import *
+
+# get always the minimum number
+print(floor(16.4))
+
+# get always the maximum number
+print(ceil(16.4))
+
+# count de sqrt
+print(sqrt(16))
