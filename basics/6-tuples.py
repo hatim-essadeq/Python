efeae6cebc2357aa084863866c60cef2f35da6d2
hatim essadeq <hatim.essadeq@gmail.com>
2021-06-17 12:45:44 +0100

    basics/6-tuples

---
+++ basics/6-tuples.py
@@ -0,0 +1,19 @@
+# declare a tuple
+example = ()
+print(example)
+
+# initialize the tuple with any value
+values = (12, "sdf", True)
+print(values)
+
+# we cannot modify/add/remove values of a tuple
+coordinates = (12, 44)
+# coordinates[0] = 1
+print(coordinates)
+
+# get value by its index
+print(values[1])
+
+# we can declare a list of tuples
+list_tuples = [(1, 2, "qsd"), (True, [1, 4])]
+print(list_tuples)
