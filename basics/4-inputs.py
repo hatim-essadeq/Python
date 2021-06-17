78ca279ae7f3dbff53805c4c2454ed68a6b1bbb3
hatim essadeq <hatim.essadeq@gmail.com>
2021-06-17 11:46:08 +0100

    basics/4-inputs

---
+++ basics/4-inputs.py
@@ -0,0 +1,10 @@
+# show input message
+input("enter your name")
+
+# store input message
+name = input("enter your name : ")
+print(name)
+
+# we getting always string as type value
+age = input("enter your age : ")
+print("my age is : " + age) # we don't have any error
